ff6aadfc9fc06c81f36a68c58bf583cfacd1276e
nithiyanandham12 <45420662+nithiyanandham12@users.noreply.github.com>
2024-08-06 11:43:22 +0530

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -6,10 +6,10 @@ company_logo = "sba.jpg"  # Replace with the path to your company logo
 app_details = [
     {"logo": "_8fd6e0cf-fde4-4843-8edd-a64337932233.jpg", "name": "PDF URL ANALYZER", "link": "https://5gyk9cpo8hvmeekfgmrqyu.streamlit.app/"},
     {"logo": "_e316abec-51da-443f-bde9-ef340c79d369.jpg", "name": "Marketing Email Genrator", "link": "https://watsonx-ai-audio-analysis.onrender.com/"},
-    {"logo": "_55bc0b3f-2019-494c-9978-bbb0129a11d8.jpg", "name": "AUDIO ANALYZER", "link": "https://watsonx-ai-audio-analysis.onrender.com/"},
     {"logo": "_a691c73b-69b6-4778-92d8-e399f8333e6b.jpg", "name": "DOCUMENT RAG ASSISTANT", "link": "https://sbainfosolutions.streamlit.app/"},
     {"logo": "_ac2912c1-3aee-4072-be42-7461421dd569.jpg", "name": "SBA SEARCH ENGINE", "link": "https://sbasearch.streamlit.app/"},
     {"logo": "_67719771-fafa-467e-b2f8-557b229d48fc.jpg", "name": "MEETING TRANSCRIPTION", "link": "https://meeting-transcription-cncvjdrgt3pd9b7adtucyw.streamlit.app/"},
+    {"logo": "_55bc0b3f-2019-494c-9978-bbb0129a11d8.jpg", "name": "AUDIO ANALYZER", "link": "https://watsonx-ai-audio-analysis.onrender.com/"},
     {"logo": "_8590e2f5-3550-4dfd-be77-1606a13e4a34.jpg", "name": "SPEECH TO TEXT", "link": "https://663e0d1381ac3e069fe00384--gilded-dieffenbachia-1bccb4.netlify.app/"},
 ]
 
