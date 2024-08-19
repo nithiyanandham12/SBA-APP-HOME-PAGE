5cbce5b375be0073fbbe5a666643e769f59ec33f
nithiyanandham12 <45420662+nithiyanandham12@users.noreply.github.com>
2024-08-19 14:24:45 +0530

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -10,7 +10,7 @@ app_details = [
     {"logo": "_ac2912c1-3aee-4072-be42-7461421dd569.jpg", "name": "SBA SEARCH ENGINE", "link": "https://sbasearch.streamlit.app/"},
     {"logo": "_071cd6cb-45c1-49d2-a3ba-a1551b11418e.jpg", "name": "Invoice Processing", "link": "https://kobel.onrender.com/"},
     {"logo": "_67719771-fafa-467e-b2f8-557b229d48fc.jpg", "name": "MEETING TRANSCRIPTION", "link": "https://meeting-transcription-cncvjdrgt3pd9b7adtucyw.streamlit.app/"},
-    {"logo": "_55bc0b3f-2019-494c-9978-bbb0129a11d8.jpg", "name": "AUDIO ANALYZER", "link": "https://watsonx-ai-audio-analysis.onrender.com/"},
+    {"logo": "_55bc0b3f-2019-494c-9978-bbb0129a11d8.jpg", "name": "AUDIO ANALYZER", "link": "https://sbainfosolutionaudioanalysis.streamlit.app/"},
     {"logo": "_8590e2f5-3550-4dfd-be77-1606a13e4a34.jpg", "name": "SPEECH TO TEXT", "link": "https://663e0d1381ac3e069fe00384--gilded-dieffenbachia-1bccb4.netlify.app/"},
 ]
 
